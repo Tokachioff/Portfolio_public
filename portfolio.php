1aacb13ef8904dc775df2598b72987f93ef94911
Tokachioff <nathagalat@orange.fr>
2025-11-10 21:35:32 +0100

    MAJ taille

--- portfolio.php
+++ portfolio.php
@@ -7,6 +7,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $destinataire = "nathagalat@orange.fr";
 
+        $recaptcha_secret = "6LeYZwgsAAAAAOYJZABK9ITRyLHrsyHcuX6xwjxm";
+        $recaptcha_response = $_POST['g-recaptcha-response'] ?? '';
+
+
+        $recaptcha = file_get_contents(
+                "https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response"
+        );
+        $recaptcha = json_decode($recaptcha);
+
+        if (!$recaptcha->success) {
+                echo "<p style='color:red;'>❌ Veuillez valider le reCAPTCHA.</p>";
+                exit;
+        }
+
     // Vérification de l'email
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "<p style='color:red;'>L'email n'est pas valide.</p>";
@@ -17,7 +31,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!empty($email) && !empty($objet) && !empty($message)) {
         if (mail($destinataire, $objet, $message, $headers)) {
-            echo "<p style='color:green;'>L'email a bien été envoyé.</p>";
+            echo "
+                <div style='text-align:center;font-family:sans-serif;margin-top:2rem;'>
+                <p style='color:green;font-size:1.2rem;'>✅ L'email a bien été envoyé.</p>
+                <p>Redirection vers le portfolio dans <span id='countdown'>5</span> secondes...</p>
+                <script>
+                let sec = 5;
+                const countdown = document.getElementById('countdown');
+                const timer = setInterval(() => {
+                sec--;
+                countdown.textContent = sec;
+                if(sec <= 0) clearInterval(timer);
+                }, 1000);
+                </script>
+                <meta http-equiv='refresh' content='5;url=https://tokachioff.github.io/Portfolio_public/main.html#contact'>
+</div>
+";
         } else {
             echo "<p style='color:red;'>Erreur lors de l'envoi du mail.</p>";
         }
